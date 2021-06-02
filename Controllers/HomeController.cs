6737be7792262063f8f0eec574785290112a66a8
bdogout <83922646+bdogout@users.noreply.github.com>
2021-06-02 11:36:13 -0400

    First commit of DemoMVCApp

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using RealMVCPrototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibraryMVC;
+using DataLibraryMVC.BusinessLogic;
+
+
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult TestScaf()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ProductCreation()
+        {
+            ViewBag.Message = "Product Creation Page.";
+
+            return View();
+        }
+
+        public ActionResult DeleteProduct(ProductModel model)
+        {
+            ViewBag.Message = "Product Creation Page.";
+
+            var productToDelete = new DataLibraryMVC.Models.ProductModel();
+
+            productToDelete.ProductSKU = model.ProductSKU;
+
+            return View(productToDelete);
+        }
+
+        public ActionResult ViewProducts()
+        {
+            ViewBag.Message = "All products.";
+
+            var data = ProductProcessor.LoadProducts();
+            List<ProductModel> products = new List<ProductModel>();
+
+            foreach (var row in data)
+            {
+                products.Add(new ProductModel
+                {
+                    ProductSKU = row.ProductSKU,
+                    ProductClassification = row.ProductClassification,
+                    ProductPrice = row.ProductPrice,
+                    ProductInStock = row.ProductInStock,
+                    ProductBrand = row.ProductBrand
+                });
+
+            }
+
+            return View(products);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ProductCreation(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int recordsCreated  = ProductProcessor.CreateProduct(model.ProductSKU, 
+                    model.ProductClassification, 
+                    model.ProductPrice, 
+                    model.ProductInStock, 
+                    model.ProductBrand);
+                
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RealMVCPrototype.Models
+{
+    public class ProductModel
+    {
+        /// <summary>
+        /// Unique identifier for a specific product.
+        /// </summary>
+        [Display(Name = "Product SKU")]
+        [Required(ErrorMessage = "You need to enter a unique SKU for this product!")]
+        public string ProductSKU { get; set; }
+
+        /// <summary>
+        /// A classification to help group product types for packaging relevant data.
+        /// </summary>
+        [Display(Name = "Product Classification")]
+        [Required(ErrorMessage = "You need to enter a classification for this product!")]
+        public string ProductClassification { get; set; }
+
+        /// <summary>
+        /// Price of one unit of a product
+        /// </summary>
+        [Display(Name = "Product Price")]
+        [Required(ErrorMessage = "You need to enter a price for this product!")]
+        public decimal ProductPrice { get; set; }
+
+        /// <summary>
+        /// Current on hand quantity if it is already known.
+        /// </summary>
+        [Display(Name = "Product On Hand Quantity")]
+        public int ProductInStock { get; set; }
+
+        /// <summary>
+        /// Associates a brand with a product for further packaging of relevant data.
+        /// </summary>
+        [Display(Name = "Product Brand")]
+        [Required(ErrorMessage = "You need to enter a Brand for this product!")]
+        public string ProductBrand { get; set; }
+    }
+}
