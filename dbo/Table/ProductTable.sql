6f5605ff8d65f421582b740ac398488970e017c7
bdogout <83922646+bdogout@users.noreply.github.com>
2021-06-02 11:56:55 -0400

    Database upload for project

---
+++ dbo/Table/ProductTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProductTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductSKU] NVARCHAR(50) NOT NULL, 
+    [ProductClassification] NVARCHAR(50) NOT NULL, 
+    [ProductPrice] MONEY NOT NULL, 
+    [ProductInStock] INT NULL, 
+    [ProductBrand] NVARCHAR(50) NOT NULL
+)
