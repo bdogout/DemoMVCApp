3b7a4aab2de7aaaac729221430ad9d0caa6ca88a
bdogout <83922646+bdogout@users.noreply.github.com>
2021-06-02 11:54:12 -0400

    Adding DemoMVCApp data library

---
+++ BusinessLogic/ProductProcessor.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using DataLibraryMVC.DataAccess;
+using DataLibraryMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibraryMVC.BusinessLogic
+{
+    public static class ProductProcessor
+    {
+        public static int CreateProduct(string productSKU, string productClassification, 
+            decimal productPrice, int productInStock, string productBrand) 
+        {
+            ProductModel data = new ProductModel()
+            {
+                ProductSKU = productSKU,
+                ProductClassification = productClassification,
+                ProductPrice = productPrice,
+                ProductInStock = productInStock,
+                ProductBrand = productBrand
+            };
+
+            string sql = @" insert into dbo.ProductTable (ProductSKU, ProductClassification, ProductPrice, ProductInStock, ProductBrand)
+                            values (@ProductSKU, @ProductClassification, @ProductPrice, @ProductInStock, @ProductBrand);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static int DeleteProduct(string productSKU)
+        {
+            var p = new DynamicParameters();
+            p.Add("@ProductSKU", productSKU); 
+            
+
+            string sql = @"delete from dbo.ProductTable
+                            where ProductSKU = @ProductSKU;";
+
+            return SqlDataAccess.DeleteData(sql, p);
+        }
+
+        public static List<ProductModel> LoadProducts() 
+        {
+            string sql = @"select Id, ProductSKU, ProductClassification, ProductPrice, ProductInStock, ProductBrand
+                        from dbo.ProductTable;";
+
+            return SqlDataAccess.LoadData<ProductModel>(sql);
+        }
+
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibraryMVC.Models
+{
+    public class ProductModel
+    {
+        public string ProductSKU { get; set; }
+        public string ProductClassification { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductInStock { get; set; }
+        public string ProductBrand { get; set; }
+
+    }
+}
